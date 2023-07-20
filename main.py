d0b5b3f477a51739054d91f30a06a8393234c38d
Tonyngor <123629176+Tonyngor@users.noreply.github.com>
2023-07-20 20:54:23 +0300

    task_12

---
+++ main.py
@@ -0,0 +1,9 @@
+s = 30
+p = 144
+for x in range (0, 1000):
+     for y in range (0, 1000):
+            if s == x + y:
+                if p == x * y:
+                     
+                     print (x)
+                     
